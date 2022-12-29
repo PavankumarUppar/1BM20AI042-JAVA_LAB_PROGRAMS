87bd2da67454dd5a78a04bac94c3dde1095d7ddf
Pavankumar Uppar <83912124+PavankumarUppar@users.noreply.github.com>
2022-12-29 22:58:32 +0530

    Create programming_exercise_6.java

---
+++ Assignments/programming_exercise_6.java
@@ -0,0 +1,11 @@
+package Assignments;
+
+import Assignments.Employee.emp;
+
+public class programming_exersice_6 {
+    public static void main(String[] args) {
+        Emp e = new Emp("ANU", "23", "Female", 12000);
+        e.calculate();
+        e.display();
+    }
+}
